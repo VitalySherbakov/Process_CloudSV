b6ee4d58c845c63b98b397d29a502fe1e44ccf89
Gendalf_Istari_2016 <palantir2018see@gmail.com>
2023-05-10 00:35:12 +0300

    42424

--- Main.py
+++ Main.py
@@ -1,15 +1,19 @@
 import os, sys, time, re, json, datetime, random
 from SettingApp import Setting
 
-print("Скрипт Успешно запущен!")
+platform_select=sys.argv[1].lower()
 
 app = Setting()
 
-while True:
-    select=input("Выберете CPU=1/GPU=2: ")
-    if select=="1":
-        print("Выбрано CPU")
-    if select=="2":
-        print("Выбрано GPU")
-    elif select!="1" and select!="2":
-        print("Не Выбрано 1 или 2")
+if platform_select=="linex":
+    while True:
+        select=input("Выберете CPU=1/GPU=2: ")
+        if select=="1":
+            print("Выбрано CPU")
+        if select=="2":
+            print("Выбрано GPU")
+        elif select!="1" and select!="2":
+            print("Не Выбрано 1 или 2")
+
+if platform_select=="service":
+    pass
