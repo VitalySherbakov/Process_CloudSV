6b824515e66e6c4a3924d055dbd6641582199ea9
Gendalf_Istari_2016 <palantir2018see@gmail.com>
2023-06-04 21:43:00 +0300

    24234

--- Linex_SV2.sh
+++ Linex_SV2.sh
@@ -175,7 +175,8 @@ function main(){
 		function_run_cpu "--help"
 	fi
 	if [ "$command" == "run" ]; then
-		python_run "Linex_Main" $1
+		echo "$1"
+		python_run "Linex_Main" "$1"
 	fi
 	if [ "$command" == "exit" ]; then
 		break
