2501c89c4fb4dfb12f2c7750b47ec42c8ff48273
Gendalf_Istari_2016 <palantir2018see@gmail.com>
2023-06-10 20:05:07 +0300

    919591

--- Linex_Main2.py
+++ Linex_Main2.py
@@ -129,6 +129,97 @@ while True:
                 #os.system(commandrun2)
     if select=="2":
         print("Выбрано GPU Ускоренный Процесс")
-        hc22000cap=app.InputWhile("Файлы hc22000, cap (dwn если надо скачать): ")
+        print("dwn - если надо скачать hc22000 или cap")
+        print("f - eсли надо указать путь к hc22000 или cap")
+        hc22000cap=app.InputWhile("Команда dwn/f: ")
+        if hc22000cap=="dwn":
+            urldwn=app.InputWhile("Url: ")
+            filepath=app.InputWhile("Имя файла hc22000 или cap: ")
+            if app.LinkValid(urldwn)==False:
+                print(f"Ссылка {urldwn} Указана Не Верно!")
+            res=app_linex.DownLoad_HC220002(urldwn, filepath)
+            filepath=res[1]
+            Accesss=app.GetFileInfo(filepath)[0]
+        if hc22000cap=="f":
+            filepath=app.InputWhile("Имя файла hc22000 или cap: ")
+            filecap=app_linex.app.SettingApp["FolderHC22000_Cap"]
+            filepath=f"{dir_path}/{filecap}/{filepath}"
+            Accesss=app.GetFileInfo(filepath)[0]
+        if Accesss:
+            print(f"Файл {filepath} Есть!")
+            print("Список Словарей 0-Все Словари")
+            print("Способ Расшыфровки 1-один словарь")
+            print("Способ Расшыфровки 2-пачка словарей")
+            print("Способ Расшыфровки 3-все словари")
+            selectdicts=app.InputWhile("Выбрать способ Расшыфровки 0,1,2,3: ")
+            if selectdicts=="0":
+                for i,li in enumerate(app_linex.GetNamesDicts()):
+                    print(f"{i}) {li}")
+            if selectdicts=="1":
+                name_dict=app.InputWhile("Укажы Имя Словаря: ")
+                #---------------Скачивание Словарей---------------
+                app_linex.DownLoad_Dicts_One(name_dict)
+                #---------------Програма CPU----------------------
+                commandsintez=app_linex.GetCommand(SelectProgram.GPU)
+                #-----------------Данные------------------
+                folderdicts=app_linex.app.SettingApp["FolderDicts"]["Folder"]
+                filecap=app_linex.app.SettingApp["FolderHC22000_Cap"]
+                #-----------------Словари------------------
+                commanddicts=""
+                listdicts=app_linex.GetFilesFindDict(name_dict)
+                for li in listdicts:
+                    commanddicts+=f'"{dir_path}/{folderdicts}/{li}" '
+                commanddicts=commanddicts[:-1]
+                #----------------------------------------
+                #-----------------Доступ-----------------
+                app_linex.Access_Folder_Linex(dir_path,SelectPlatform.NONE) #Доступ
+                #----------------------------------------
+                commandrun2=f'{commandsintez[0]} -m 22000 -a 0 -w 1 "{filepath}" {commanddicts}'
+                print(commandrun2)
+                os.system(commandrun2)
+            if selectdicts=="2":
+                number_dict=app.InputWhile("Укажы Номер Пачки Словарей 1-3: ")
+                #---------------Скачивание Словарей---------------
+                number_dict=int(number_dict)
+                app_linex.DownLoad_Dicts_Pack(number_dict)
+                #---------------Програма CPU----------------------
+                commandsintez=app_linex.GetCommand(SelectProgram.GPU)
+                #-----------------Данные------------------
+                folderdicts=app_linex.app.SettingApp["FolderDicts"]["Folder"]
+                filecap=app_linex.app.SettingApp["FolderHC22000_Cap"]
+                #-----------------Словари------------------
+                commanddicts=""
+                listdicts=app_linex.GetFilesPackDict(number_dict)
+                for li in listdicts:
+                    commanddicts+=f'"{dir_path}/{folderdicts}/{li}" '
+                commanddicts=commanddicts[:-1]
+                #----------------------------------------
+                #-----------------Доступ-----------------
+                app_linex.Access_Folder_Linex(dir_path,SelectPlatform.NONE) #Доступ
+                #----------------------------------------
+                commandrun2=f'{commandsintez[0]} -m 22000 -a 0 -w 1 "{filepath}" {commanddicts}'
+                print(commandrun2)
+                #os.system(commandrun2)
+            if selectdicts=="3":
+                #---------------Скачивание Словарей---------------
+                app_linex.DownLoad_Dicts_All()
+                #---------------Програма CPU----------------------
+                commandsintez=app_linex.GetCommand(SelectProgram.GPU)
+                #-----------------Данные------------------
+                folderdicts=app_linex.app.SettingApp["FolderDicts"]["Folder"]
+                filecap=app_linex.app.SettingApp["FolderHC22000_Cap"]
+                #-----------------Словари------------------
+                commanddicts=""
+                listdicts=app_linex.GetFilesAllDict()
+                for li in listdicts:
+                    commanddicts+=f'"{dir_path}/{folderdicts}/{li}" '
+                commanddicts=commanddicts[:-1]
+                #----------------------------------------
+                #-----------------Доступ-----------------
+                app_linex.Access_Folder_Linex(dir_path,SelectPlatform.NONE) #Доступ
+                #----------------------------------------
+                commandrun2=f'{commandsintez[0]} -m 22000 -a 0 -w 1 "{filepath}" {commanddicts}'
+                print(commandrun2)
+                #os.system(commandrun2)
     elif select!="1" and select!="2":
         print("Не Выбрано 1 или 2")
