48d6da70a46fc1fce1d0dfec37ef7bc067160577
Gendalf_Istari_2016 <palantir2018see@gmail.com>
2023-06-14 21:53:14 +0300

    51516

--- App_Process2.py
+++ App_Process2.py
@@ -133,7 +133,10 @@ class AppProcessLinex(object):
             file=self.app.SettingApp["Urls_GPU_CPU_Full"][1]["File"]
         downprogram=f"{dir_path}/{file}" #загрузка програмы
         dirextract=f"{dir_path}/{dirpath}" #распаковка
-        print(downprogram)
+        print(f"D: {dir_path}")
+        print(f"F: {file}")
+        print(f"D1: {dirpath}")
+        print(f"P: {downprogram}")
         Flag=self.app.DownloadFile(url,downprogram)
         if Flag:
             self.arh.ExtractFull(downprogram,dirextract,SelectArhive.TAR)
